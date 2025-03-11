54fd1d48ae51d6cb3c41b58e3d4f07bab4bdcf64
Tayyib Ul Hassan <tayyibgondal2003@gmail.com>
2025-03-11 19:18:22 +0500

    fix: add session import in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, flash, send_from_directory, jsonify
+from flask import Flask, render_template, redirect, url_for, request, flash, send_from_directory, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
